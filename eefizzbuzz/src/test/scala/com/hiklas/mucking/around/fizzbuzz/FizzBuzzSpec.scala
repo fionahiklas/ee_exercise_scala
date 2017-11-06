f7aea9be017a7635c6e327fc12cc3f1e03824989
Fiona Bianchi <fiona@hiklas.co.uk>
2017-11-06 07:19:58 +0000

    Changed the code over to use the case class for counting output

--- eefizzbuzz/src/test/scala/com/hiklas/mucking/around/fizzbuzz/FizzBuzzSpec.scala
+++ eefizzbuzz/src/test/scala/com/hiklas/mucking/around/fizzbuzz/FizzBuzzSpec.scala
@@ -41,28 +41,12 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
     assert( fizzBuzz.checkOneNumber(3) == "luck")
   }
 
-
-  "Check sum of counts" should "return all zeros for 0" in {
-    val fizzBuzz = fixture.fizzbuzzToTest
-
-    val result = fizzBuzz.sumCount((0,0,0,0,0), (0,0,0,0,0))
-    assert( result == (0,0,0,0,0))
-  }
-
-  it should "return all correct sum for random test numbers" in {
-    val fizzBuzz = fixture.fizzbuzzToTest
-
-    val result = fizzBuzz.sumCount((4,9,42,3,18), (132,17,10000,53,93))
-    assert( result == (136,26,10042,56,111))
-  }
-
-
   "Check processAndSumList" should "return empty list and all zeros for sum for empty list input" in {
     val fizzBuzz = fixture.fizzbuzzToTest
 
     val (list, totals) = fizzBuzz.processAndSumList(List[Int]())
     assert( list == List[String]() )
-    assert( totals == (0,0,0,0,0) )
+    assert( totals == FizzBuzz.OutputCounter(0,0,0,0,0) )
   }
 
   it should "return one list item and simple totals for one input item" in {
@@ -72,9 +56,28 @@ class FizzBuzzSpec extends FlatSpec with Matchers {
     val (list, totals) = fizzBuzz.processAndSumList(testList)
 
     assert( list == "2" :: Nil )
-    assert( totals == (0,0,0,0,1) )
+    assert( totals == FizzBuzz.OutputCounter(0,0,0,0,1) )
   }
 
 
+  "Check outputCounter" should "new object return 0 for each field" in {
 
+    val result = FizzBuzz.OutputCounter(0,0,0,0,0)
+
+    assert(result.fizz == 0)
+    assert(result.buzz == 0)
+    assert(result.fizzbuzz == 0)
+    assert(result.luck == 0)
+    assert(result.digit == 0)
+  }
+
+  it should "Sum two objects correctly" in {
+
+    val left = FizzBuzz.OutputCounter(4,9,42,3,18)
+    val right = FizzBuzz.OutputCounter(132,17,10000,53,93)
+
+    val result = left + right
+
+    assert( result == FizzBuzz.OutputCounter(136,26,10042,56,111))
+  }
 }
